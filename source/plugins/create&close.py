5fa7f5620920bbfbe2ba5c1850e2f659c2d6ee16
SHADOW ALZAYEEM <116435902+BELALSHADOW19@users.noreply.github.com>
2023-04-26 00:25:38 +0200

    Update create&close.py

--- source/plugins/create&close.py
+++ source/plugins/create&close.py
@@ -28,7 +28,7 @@ async def get_group_call(
     await message.edit(f"{err_msg}")
     return False
 
-@Client.on_message(filters.command("فتح الكول$", prefixes=f".") & (filters.me | filters.user(sudo_command))
+@Client.on_message(filters.command("فتح الكول$", prefixes=f".") & (filters.me | filters.user(sudo_command)))
 async def opengc(c, msg):
     await msg.edit("جاري فتح الكول")
     if (
